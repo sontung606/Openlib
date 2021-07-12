78dcc85dab8d98f683bba336a27881d722d5ff8c
SONTUNG\SonTung <sontung606@gmail.com>
2021-07-12 09:25:16 +0700

    thêm chức năng mượn book

--- openLib/routes/libRoutes/book.js
+++ openLib/routes/libRoutes/book.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const book = require('../../controller/book');
+const isLogin = require('../../middleware/is-login')
 
-router.get('/book-details/:bookID',book.getbook)
+router.get('/book-details/:bookID',book.getbook);
+router.get('/book-borrow/:bookID',isLogin,book.getBookBorrow);
+router.post('/book-borrow',isLogin,book.postBookBorrow);
 
 module.exports=router;
